05b5a7eb6dd85364b4a65622693d35a11eecc174
Bhanu707 <Bhanu.maniktahla@pyramidconsultinginc.com>
2021-09-19 18:00:25 +0530

    react-js-to-ts

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Home from "./Home/index"
 function App() {
   return (
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import  App from './App';
 import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from 'reportWebVitals';
 
-var state = { hasError: false };
+//var state = { hasError: false };
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),()=>{if (state.hasError) {
+  document.getElementById('root')
+  //,()=>{if (state.hasError) {
     // You can render any custom fallback UI
-    return <h1>Something went wrong.</h1>;
-  }}
+    //return <h1>Something went wrong.</h1>;
+//  }
+//}
 );
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals("any");
+
+
