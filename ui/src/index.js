ef4cc8581f057767767ec49d4cfd9cd10b1c738a
Bhanu707 <Bhanu.maniktahla@pyramidconsultinginc.com>
2021-09-19 13:37:10 +0530

    added fix for error bpoundary react review

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
+var state = { hasError: false };
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),()=>{if (state.hasError) {
+    // You can render any custom fallback UI
+    return <h1>Something went wrong.</h1>;
+  }}
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
