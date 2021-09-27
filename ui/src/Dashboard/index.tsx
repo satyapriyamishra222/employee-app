24c7119ceb1231587405fd35f0ea107a364ff859
Bhanu707 <Bhanu.maniktahla@pyramidconsultinginc.com>
2021-09-27 15:26:35 +0530

    table with static data

--- ui/src/Dashboard/index.tsx
+++ ui/src/Dashboard/index.tsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react'
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-// import loginSer from "./login.service";
-// import authSer from "../shared/service/auth";
-
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import { useHistory } from "react-router-dom";
 import Home from '../Home';
 import { useTranslation } from 'react-i18next';
-
-//import './login.css';
 export default function Login(props: any): any {
     const { t, i18n } = useTranslation();
     let history = useHistory();
@@ -31,23 +26,14 @@ export default function Login(props: any): any {
 
     const redirectDashbord = () => {
         props.LoginFun();
-        let url = "/home"
-        history.push(url)
+        history.push("/home");
     }
-    // useEffect(() => {
-    //     if (localStorage.getItem('user-info')) {
-    //         history.push("/home");
-    //     }
-    // }, [])
-
 
     const isVlaidLogin = () => {
         if (state.email === 'admin' && state.pass === "admin") {
-            // console.log("valid user")
             redirectDashbord()
             return true
         }
-        // console.log("Not valid user")
         return false
     }
 
@@ -55,14 +41,6 @@ export default function Login(props: any): any {
     const handleLogin = function (e: any) {
         e.preventDefault();
         isVlaidLogin()
-        //     loginSer.login(state).then((res) => {
-        //         console.log(res);
-        //         authSer.login(res);
-        //         //    authSer.logout(res);    
-        //     }).catch((error) => {
-        //         console.log(error.message);
-        //     })
-        // }
     }
     return (<>
         <Grid container className="h-100 login-wrp" direction="row" >
@@ -77,7 +55,7 @@ export default function Login(props: any): any {
             </Grid>
             <Grid item xs={6}>
                 <Grid container justifyContent="center" alignItems="center" className="h-100" direction="column">
-                    <h1 className="login-title"> Login</h1>
+                    <h1 className="login-title">{t(`intro.label2`)}</h1>
                     <form onSubmit={handleLogin}>
                         <FormControl>
                             <TextField value={state.email} placeholder="Email" onChange={handleEmail} id="email" size="small" variant="outlined" />
@@ -86,7 +64,7 @@ export default function Login(props: any): any {
                             <TextField value={state.pass} id="pass" type="password" placeholder="password" onChange={handlePass} size="small" variant="outlined" />
                         </FormControl>
 
-                        <Button variant="contained" color="primary" type="submit" className="w-100"> Login </Button>
+                        <Button variant="contained" color="primary" type="submit" className="w-100"> {t(`intro.label1`)}</Button>
                     </form>
                 </Grid>
             </Grid>
